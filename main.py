1d7e769308f3ee87a22886d601842ee19058d741
do.thang <dowindev26@gmail.com>
2025-03-24 00:44:35 +0700

    update endpoint swagger

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from fastapi import FastAPI
 from routes.chat_router import router as chat_router
 from routes.suggestion_route import router as suggestion_router
 
-app = FastAPI()
+app = FastAPI(
+    title="My API",
+    description="This is a sample API",
+    version="1.0",
+    docs_url="api/ai/swagger-ui.html",
+    redoc_url="/redocs"
+)
 
 app.include_router(suggestion_router)
 app.include_router(chat_router)
