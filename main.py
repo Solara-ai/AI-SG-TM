ca854fe8632329a89e026ca25be289e5f26fc0ab
do.thang <dowindev26@gmail.com>
2025-03-24 17:55:37 +0700

    update swagger and create deploy.yml

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from routes.chat_router import router as chat_router
 from routes.suggestion_route import router as suggestion_router
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/swagger-ui.html",  # Thay đổi đường dẫn Swagger UI
+    redoc_url=None,  # (Tùy chọn) Tắt Redoc nếu không cần
+    openapi_url="/openapi.json"  # (Tùy chọn) Thay đổi OpenAPI JSON
+)
 
 app.include_router(suggestion_router)
 app.include_router(chat_router)
