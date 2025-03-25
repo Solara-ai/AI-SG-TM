b2069a207ed19a43582757edbe302380d2458034
Datghb <nguyenthanhdat0124@gmail.com>
2025-03-25 20:36:37 +0700

    add_middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from routes.statistics_router import router as statistics_router
 from routes.chat_router import router as chat_router
@@ -13,3 +14,11 @@ app = FastAPI(
 app.include_router(suggestion_router)
 app.include_router(chat_router)
 app.include_router(statistics_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho phép tất cả FE gọi API (hoặc thay bằng domain cụ thể)
+    allow_credentials=True,
+    allow_methods=["*"],  # Cho phép tất cả phương thức (GET, POST, PUT, DELETE)
+    allow_headers=["*"],  # Cho phép tất cả header
+)
